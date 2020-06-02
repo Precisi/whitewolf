a3fb0c9afa288b6775cc244df305badeaa7ddb26
Precisi <pauline@closetgamers.com>
2020-06-02 15:10:55 -0400

    Initial commit

---
+++ module/actor.js
@@ -0,0 +1,329 @@
+/**
+ * Extend the base Actor entity by defining custom roll structures.
+ * @extends {Actor}
+ */
+
+export class ActorCoD extends Actor {
+	prepareData() {
+		super.prepareData();
+
+		const actorData = this.data;
+		const data = actorData.data;
+		const flags = actorData.flags;
+
+		// Make separate methods for each Actor type (character, npc, etc.) to keep
+		// things organized.
+		if (actorData.type === 'character') this._prepareCharacterData(actorData);
+	}
+
+	// Prepare character data
+	_prepareCharacterData(actorData) {
+		const data = actorData.data;
+		const attributes = duplicate(CONFIG.attributes);
+		const skills = duplicate(CONFIG.skills);
+
+		const hp = data.advantages.hp;
+		const sta = data.attributes.sta;
+		const size = data.advantages.size;
+
+		const wp = data.advantages.wp;
+		const res = data.attributes.res;
+		const com = data.attributes.com;
+
+		const def = data.advantages.def;
+		const dex = data.attributes.dex;
+		const wits = data.attributes.wits;
+		const athletics = data.skills.athletics;
+
+		const init = data.advantages.init;
+		const integ = data.advantages.integ;
+
+		const speed = data.advantages.speed;
+		const str = data.attributes.str;
+		const garmor = data.advantages.garmor;
+		const barmor = data.advantages.barmor;
+		const exp = data.advancement.exp;
+		const beats = data.advancement.beats;
+
+		// Check to see if current HP/WP > max, correct if so
+		if (hp.value > hp.max) hp.value = hp.max;
+		if (hp.value < 0) hp.value = 0;
+		if (wp.value > wp.max) wp.value = wp.max;
+		if (wp.value < 0) wp.value = 0;
+
+		// Ensure Intrgrity is valid
+		if (integ.value < integ.min) integ.value = integ.min;
+		if (integ.value > integ.max) integ.value = integ.max;
+
+		// Ensure Attributes and Skills are appropriate values
+		for (let a in attributes) {
+			if (data.attributes[a].value > data.attributes[a].max)
+				data.attributes[a].value = data.attributes[a].max;
+			if (data.attributes[a].value < data.attributes[a].min)
+				data.attributes[a].value = data.attributes[a].min;
+		}
+
+		for (let s in skills) {
+			if (data.skills[s].value > data.skills[s].max)
+				data.skills[s].value = data.skills[s].max;
+			if (data.skills[s].value < data.skills[s].min)
+				data.skills[s].value = data.skills[s].min;
+		}
+
+		// Ensure others are valid
+		if (size.value < size.min) size.value = size.min;
+		if (garmor.value < garmor.min) garmor.value = garmor.min;
+		if (barmor.value < barmor.min) barmor.value = barmor.min;
+		if (exp.value < exp.min) exp.value = exp.min;
+		if (beats.value < beats.min) beats.value = beats.min;
+
+		// Using correct values, define derived character values
+		hp.max = sta.value + size.value;
+		wp.max = res.value + com.value;
+		def.value = Math.min(wits.value, dex.value) + athletics.value;
+		init.value = dex.value + com.value;
+		speed.value = str.value + dex.value + 5;
+		integ.max = 10;
+		if (beats.value >= 5) {
+			exp.value += Math.round(beats.value / 5);
+			beats.value = beats.value % 5;
+		}
+	}
+
+	rollPool(attribute, skill, modifier, exploder) {
+		// Ex: 'int', 'animalken', 'ten'. Define global roll pool, assume valid att
+		// & skill sent even if 0 or negative value.
+		let name = this.name;
+		let pool = 0;
+		let attVal = parseInt(this.data.data.attributes[attribute].value, 10);
+		let skillVal = parseInt(this.data.data.skills[skill].value, 10);
+		let attGroup = CONFIG.groupMapping[attribute];
+		let skillGroup = CONFIG.groupMapping[skill];
+		let modVal = parseInt(modifier, 10) || 0;
+		let penalty = 0;
+		let penaltyStr = '';
+		let explodeStr = 'x10';
+
+		// Determine if inadequate skill
+		if (skillVal < 1) {
+			if (skillGroup === 'mental') penalty = -3;
+			else penalty = -1;
+		}
+		if (penalty < 0)
+			penaltyStr = `<br> Insufficient skill! Penalty of ${penalty}`;
+
+		// Determine exploder
+		switch (exploder) {
+			case 'none':
+				explodeStr = '';
+				break;
+			case 'ten':
+				explodeStr = 'x10';
+				break;
+			case 'nine':
+				explodeStr = 'x>=9';
+				break;
+			case 'eight':
+				explodeStr = 'x>=8';
+				break;
+		}
+
+		console.log(`--------------`);
+		console.log(
+			`Initial roll pool: ${attribute} : ${attVal}, ${skill} : ${skillVal}, Mod : ${modVal}`
+		);
+		console.log(`Att: ${attGroup}, Skill: ${skillGroup}`);
+		console.log(`Exploder: ${exploder}`);
+		if (penalty < 0) console.log(`Penalty assessed: ${penalty}`);
+		console.log(`--------------`);
+
+		// Determine final roll pool
+		pool = pool + attVal + skillVal + modVal + penalty;
+		console.log(`Final roll pool: ${pool}`);
+		console.log(`--------------`);
+
+		if (pool > 0) {
+			// Regular roll
+			let roll = new Roll(`${pool}d10${explodeStr}cs>=8`).roll();
+			roll.toMessage({
+				flavor: `<b>${name}</b> makes a roll!<br>
+					${CONFIG.attributes[attribute]}: ${attVal}<br>
+					${CONFIG.skills[skill]}: ${skillVal}<br>
+					Modifier: ${modVal}${penaltyStr}`,
+			});
+		} else {
+			// Chance roll
+			let roll = new Roll(`1d10cs=10`).roll();
+			roll.toMessage({
+				flavor: `<b>${name}</b> makes a roll!<br>
+				${CONFIG.attributes[attribute]}: ${attVal}<br>
+				${CONFIG.skills[skill]}: ${skillVal}<br> 
+				Modifier: ${modVal} ${penaltyStr}<br>
+			  Roll is reduced to a chance die!`,
+			});
+		}
+	}
+
+	attTaskPool(attribute1, attribute2, modifier, exploder) {
+		// Ex: 'wits', 'composure', 'ten'. Define global roll pool, assume 2 valid att's sent even if 0 or negative value.
+		let name = this.name;
+		let pool = 0;
+		let att1Val;
+		let att2Val;
+		let att1String;
+		let att2String;
+
+		if (attribute1 == 'none') {
+			att1Val = 0;
+			att1String = 'None';
+		} else {
+			att1Val = parseInt(this.data.data.attributes[attribute1].value, 10);
+			att1String = `${CONFIG.attributes[attribute1]}: ${att1Val}`;
+		}
+
+		if (attribute2 == 'none') {
+			att2Val = 0;
+			att2String = 'None';
+		} else {
+			att2Val = parseInt(this.data.data.attributes[attribute2].value, 10);
+			att2String = `${CONFIG.attributes[attribute2]}: ${att2Val}`;
+		}
+
+		let att1Group = CONFIG.groupMapping[attribute1];
+		let att2Group = CONFIG.groupMapping[attribute2];
+		let modVal = parseInt(modifier, 10) || 0;
+		let explodeStr = 'x10';
+
+		// Determine exploder
+		switch (exploder) {
+			case 'none':
+				explodeStr = '';
+				break;
+			case 'ten':
+				explodeStr = 'x10';
+				break;
+			case 'nine':
+				explodeStr = 'x>=9';
+				break;
+			case 'eight':
+				explodeStr = 'x>=8';
+				break;
+		}
+
+		console.log(`--------------`);
+		console.log(
+			`Initial roll pool: ${attribute1} : ${att1Val}, ${attribute2} : ${att2Val}, Mod : ${modVal}`
+		);
+		console.log(
+			`Attribute 1 Group: ${att1Group}, Attribute 2 Group: ${att2Group}`
+		);
+		console.log(`Exploder: ${exploder}`);
+		console.log(`--------------`);
+
+		// Determine final roll pool
+		pool = pool + att1Val + att2Val + modVal;
+		console.log(`Final roll pool: ${pool}`);
+		console.log(`--------------`);
+
+		if (pool > 0) {
+			// Regular roll
+			let roll = new Roll(`${pool}d10${explodeStr}cs>=8`).roll();
+			roll.toMessage({
+				flavor: `<b>${name}</b> makes an Attribute roll!<br>
+				${att1String}<br>
+				${att2String}<br>
+				Modifier: ${modVal}`,
+			});
+		} else {
+			// Chance roll
+			let roll = new Roll(`1d10cs=10`).roll();
+			roll.toMessage({
+				flavor: `<b>${name}</b> makes an Attribute roll!<br>
+				${att1String}<br>
+				${att2String}<br>
+				Modifier: ${modVal}<br>
+			  Roll is reduced to a chance die!`,
+			});
+		}
+	}
+
+	weaponRollPool(attribute, skill, modifier, exploder, weapon, damage, target) {
+		// Ex: 'int', 'animalken', -1, 'ten', 'Axe', 'Ben'. Define global roll pool, assume valid att & skill sent even if 0 or negative value.
+		let name = this.name;
+		let pool = 0;
+		let attVal = parseInt(this.data.data.attributes[attribute].value, 10);
+		let skillVal = parseInt(this.data.data.skills[skill].value, 10);
+		let attGroup = CONFIG.groupMapping[attribute];
+		let skillGroup = CONFIG.groupMapping[skill];
+		let modVal = parseInt(modifier, 10) || 0;
+		let penalty = 0;
+		let penaltyStr = '';
+		let explodeStr = 'x10';
+		let rollString;
+
+		// Determine if inadequate skill
+		if (skillVal < 1) {
+			if (skillGroup === 'mental') penalty = -3;
+			else penalty = -1;
+		}
+		if (penalty < 0)
+			penaltyStr = `<br> Insufficient skill! Penalty of ${penalty}`;
+
+		// Determine exploder
+		switch (exploder) {
+			case 'none':
+				explodeStr = '';
+				break;
+			case 'ten':
+				explodeStr = 'x10';
+				break;
+			case 'nine':
+				explodeStr = 'x>=9';
+				break;
+			case 'eight':
+				explodeStr = 'x>=8';
+				break;
+		}
+		console.log(`Weapon roll: ${attribute}, ${skill}`);
+		console.log(`Target: ${target}`);
+		if (target == 'none')
+			rollString = `<b>${name}</b> attacks with <b>${weapon}</b>! (${damage} dmg)`;
+		else
+			rollString = `<b>${name}</b> attacks <b>${target}</b> with <b>${weapon}</b>! (${damage} dmg)`;
+
+		console.log(`--------------`);
+		console.log(
+			`Initial roll pool: ${attribute} : ${attVal}, ${skill} : ${skillVal}, Mod : ${modVal}`
+		);
+		console.log(`Att: ${attGroup}, Skill: ${skillGroup}`);
+		console.log(`Exploder: ${exploder}`);
+		if (penalty < 0) console.log(`Penalty assessed: ${penalty}`);
+		console.log(`--------------`);
+
+		// Determine final roll pool
+		pool = pool + attVal + skillVal + modVal + penalty;
+		console.log(`Final roll pool: ${pool}`);
+		console.log(`--------------`);
+
+		if (pool > 0) {
+			// Regular roll
+			let roll = new Roll(`${pool}d10${explodeStr}cs>=8`).roll();
+			roll.toMessage({
+				flavor: `${rollString}<br>
+					${CONFIG.attributes[attribute]}: ${attVal}<br>
+					${CONFIG.skills[skill]}: ${skillVal}<br>
+					Modifier: ${modVal}${penaltyStr}`,
+			});
+		} else {
+			// Chance roll
+			let roll = new Roll(`1d10cs=10`).roll();
+			roll.toMessage({
+				flavor: `${rollString}<br>
+				${CONFIG.attributes[attribute]}: ${attVal}<br>
+				${CONFIG.skills[skill]}: ${skillVal}<br> 
+				Modifier: ${modVal} ${penaltyStr}<br>
+			  Roll is reduced to a chance die!`,
+			});
+		}
+	}
+}
---
+++ module/item-sheet.js
@@ -0,0 +1,45 @@
+/**
+ * Extend the basic ItemSheet
+ * @extends {ItemSheet}
+ */
+
+export class CoDItemSheet extends ItemSheet {
+	// Extend and override the default options
+
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.tabs = [
+			{
+				navSelector: '.tabs',
+				contentSelector: '.content',
+				initial: 'attributes',
+			},
+		];
+		options.classes = options.classes.concat(['cod', 'item-sheet']);
+		options.template = 'systems/whitewolf/templates/items/item-sheet.html';
+		options.height = 450;
+		return options;
+	}
+
+	//Use a type-specific template for each different item type
+
+	get template() {
+		let type = this.item.type;
+		return `systems/whitewolf/templates/items/item-${type}-sheet.html`;
+	}
+
+	getData() {
+		const data = super.getData();
+		if (this.item.type == 'weapon') data['attacks'] = CONFIG.attacks;
+		if (this.item.type == 'merit')
+			data['meritGroups'] = CONFIG.universalMeritGroups;
+		return data;
+	}
+
+	// Activate event listeners using the prepared sheet HTML
+	//* @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
+
+	activateListeners(html) {
+		super.activateListeners(html);
+	}
+}
